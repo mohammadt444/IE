7585c104280933932ef40c0f26cdacb509cfe94d
Mohammad Tollabi <mohammad.t.0290@gmail.com>
2021-07-20 14:52:21 +0430

    feat: Add profile

--- frontend/src/app/pages/profile/profile.jsx
+++ frontend/src/app/pages/profile/profile.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./profile.css";
 import { toPersian } from "../../../helper/functions/utils";
 import Info from "./components/info/info";
+import Receipt from "./components/receipt/receipt";
 
 function Profile() {
   return (
@@ -18,7 +19,8 @@ function Profile() {
         <label className="profile-tab profile-tab_enabled">پروفایل</label>
         <label className="profile-tab">رسید ها</label>
       </div>
-      <Info className="profile-info" />
+      {/* <Info/> */}
+      <Receipt />
     </div>
   );
 }
