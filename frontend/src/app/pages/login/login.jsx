0b5b2bb0cd250635e103f1b528f45e94c521b8f2
Hadi Ebrahimi <hadiebrahimig@gmail.com>
2021-07-20 16:14:59 +0200

    add login page

---
+++ frontend/src/app/pages/login/login.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Input from "../input/input";
+import "./sign_in.css";
+
+
+function Login() {
+    return (
+        <div className = "login">
+            <div className = "login-inputs">
+                <p className = "login-titr">فروشگاه - ثبت نام</p>
+                <div className = "login-inputs">
+                    <Input
+                            type="email"
+                            text="ایمیل"
+                            placeholder=""
+                    />
+                    <Input
+                            type="password"
+                            text="رمز عبور"
+                            placeholder=""
+                    />
+                </div>
+                <div className = "login-buttonBox">
+                    <button className = "login-button">ورود</button>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Login;
