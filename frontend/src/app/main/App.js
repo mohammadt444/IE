02f987aefb5525b89009ac132cc6263c79e29d53
Mohammad Tollabi <mohammad.t.0290@gmail.com>
2021-07-20 17:59:43 +0430

    feat: improve App.js

--- frontend/src/app/main/App.js
+++ frontend/src/app/main/App.js
@@ -1,6 +1,11 @@
 import Footer from "./components/footer/footer";
 import Header from "./components/header/header";
-import Input from "./components/input/input";
+import SearchPanel from "./components/searchPanel/searchPanel";
+import ProductBox from "./components/productBox/productBox";
+import SortBox from "./components/sortBox/sortBox";
+import Profile from "../pages/profile/profile";
+import CategoryPanel from "./components/categoryPanel/categoryPanel";
+import Home from "../pages/home/home";
 
 function App() {
   return (
@@ -20,22 +25,19 @@ function App() {
           height: "100%",
           maxWidth: 1280,
           width: "100%",
+          backgroundColor: "rgb(247, 247, 247)",
         }}
       >
         <Header />
-        <div style={{ display: "flex", flexDirection: "column", flexGrow: 1 }}>
-          <div style={{ display: "flex", flexDirection: "row" }}>
-            <Input
-              type="email"
-              text="لطفا ایمیل خود را وادر کنید"
-              placeholder="salam"
-            />
-            <Input
-              type="email"
-              text="لطفا ایمیل خود را وادر کنید"
-              placeholder="salam"
-            />
-          </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            flexGrow: 1,
+            overflow: "auto",
+          }}
+        >
+          <Home />
         </div>
         <Footer />
       </div>
