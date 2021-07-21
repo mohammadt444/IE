f826091bc0216f7e2065f6b79005288c753cb0f9
Mohammad Tollabi <mohammad.t.0290@gmail.com>
2021-07-21 14:25:44 +0430

    feat: Add slider

--- frontend/src/app/main/components/searchPanel/searchPanel.jsx
+++ frontend/src/app/main/components/searchPanel/searchPanel.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./searchPanel.css";
 
+const pics = ["/assets/clock.png", "/assets/giftBox.png", "/assets/awning.png"];
+
 function SearchPanel() {
+  const [picNumber, setPicNumber] = useState(2);
+  setInterval(() => {
+    setPicNumber((picNumber + 1) % pics.length);
+  }, 10000);
   return (
     <div className="searchPanel">
       <label className="searchPanel-header">در محصولات سایت جستجو کنید..</label>
@@ -10,7 +16,25 @@ function SearchPanel() {
         className="searchPanel-input"
       />
       <button className="searchPanel-button">جستجو کنید</button>
-      <img src="/assets/clock.png" alt="clock" />
+      <img className="searchPanel-img" src={pics[picNumber]} alt="clock" />
+      <div
+        onClick={() =>
+          setPicNumber(picNumber === 0 ? pics.length - 1 : picNumber - 1)
+        }
+        className="searchPanel-img_button searchPanel-img_button_previous"
+      >
+        <img
+          className="searchPanel-img_button_previous_svg"
+          src="/assets/svg/next.svg"
+          alt=""
+        />
+      </div>
+      <div
+        onClick={() => setPicNumber((picNumber + 1) % pics.length)}
+        className="searchPanel-img_button searchPanel-img_button_next"
+      >
+        <img src="/assets/svg/next.svg" alt="" />
+      </div>
     </div>
   );
 }
